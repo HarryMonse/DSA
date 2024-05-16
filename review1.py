6e782135026161159ee7b4b9d2c32559f3b25be4
Harry Monse <harry.monse1812@gmail.com>
2024-05-16 19:34:27 +0530

    review question solution

--- review1.py
+++ review1.py
@@ -7,17 +7,37 @@
 #     def __init__(self):
 #         self.head = None
 
-#     def delete_middle(self, x):
+#     def delete_middle(self, x):                       # Wrong Answer
 #         n = self.head
 #         while n.nref is not None:
 #             new_node = Node()
 #             n.nref = new_node
 
 
-# arr = []
 
+
+
+# TWO SUM
+
+
+# arr = []                                              # Wrong Answer                                        
 # for i in len(arr) - 1 :
 #     for j in len(arr):
 #         if i + j = value:
 
 # return i
+
+
+
+# nums = [2, 7, 11, 15]                                 # Correct Answer
+# target = 9
+
+# arr = []
+# for i in range(len(nums)-1):
+#     for j in range(i+1, len(nums)):
+#         if nums[i] + nums[j] == target:
+#             arr.append(i)
+#             arr.append(j)
+#             break 
+# print(arr)
+
