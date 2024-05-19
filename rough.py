6dd71589c2276979f4e0cc3daf6231fe8ffcb3a4
Harry Monse <harry.monse1812@gmail.com>
2024-05-20 03:21:56 +0530

    binary search practice

--- rough.py
+++ rough.py
@@ -96,3 +96,29 @@
 # print(ans)
 
 
+
+
+# def binary_search(arr, target):                                 # Binary Search
+#     low = 0 
+#     high = len(arr) -1
+
+#     while low <= high:
+#         mid = (low + high) // 2
+
+#         if arr[mid] == target:
+#             return mid
+#         else:
+#             if arr[mid] < target:
+#                 low = mid + 1
+#             else:
+#                 high = mid - 1
+#     return -1
+
+# arr = [2,3,5,7,8,10]
+# target = 5
+
+# ans = binary_search(arr, target)
+# print(ans)
+    
+
+
