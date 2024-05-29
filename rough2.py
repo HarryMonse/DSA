75d80fed959fc8e87ea52ae1bdcd86a3e268a218
Harry Monse <harry.monse1812@gmail.com>
2024-05-30 02:32:56 +0530

    all set

--- rough2.py
+++ rough2.py
@@ -1,16 +1,16 @@
-# Number Plate
+# # Number Plate
 
-ans = []
-res = 0
-result = 0
-for i in range(6200,8600):
-    for j in str(i):
-        res += int(j)
-    for k in str(res):
-        result += int(k) 
-    if result == 1: #or result == 3:  
-        ans.append(i)
-    res =0
-    result = 0
-print(ans)
+# ans = []
+# res = 0
+# result = 0
+# for i in range(6200,8600):
+#     for j in str(i):
+#         res += int(j)
+#     for k in str(res):
+#         result += int(k) 
+#     if result == 1: #or result == 3:  
+#         ans.append(i)
+#     res =0
+#     result = 0
+# print(ans)
 
