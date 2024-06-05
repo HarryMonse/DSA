db4c5b5b0f62199e186c35892b0b4f85b6baa88b
Harry Monse <harry.monse1812@gmail.com>
2024-06-05 23:54:39 +0530

    implementing stack using list

---
+++ stack_and_queues.py
@@ -0,0 +1,11 @@
+stack = []                                          # Implementing stack using list
+
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
