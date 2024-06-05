27bc43450f42396fa3156f77090bc1e3c04b4e1c
Harry Monse <harry.monse1812@gmail.com>
2024-06-06 02:31:16 +0530

    implementation of stack using list completed

--- stack_and_queues.py
+++ stack_and_queues.py
@@ -1,11 +1,55 @@
-stack = []                                          # Implementing stack using list
+# stack = []                                          # Implementing stack using list
 
-stack.append(10)
-stack.append(20)
-stack.append(30)
+# stack.append(10)
+# stack.append(20)
+# stack.append(30)
 
-print(stack)
+# print(stack)
 
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
+# print(stack.pop())
+# print(stack.pop())
+# print(stack.pop())
+
+
+# print(len(stack) == 0)                              # isEmpty 
+# print(not stack)
+
+
+# stack.append(10)                                    # Peek or Top element
+# stack.append(20)
+# stack.append(30)
+# print(stack[-1])                                    
+
+
+
+# stack= []                                           #Program
+
+# def push():
+#     if len(stack)==n:
+#         print("list is full!")
+#     else:
+#         element = input("Enter the element: ")
+#         stack.append(element)
+#         print(stack)
+
+# def pop_element():
+#     if not stack:
+#         print ("stack is empty!")
+#     else:
+#         e = stack.pop()
+#         print("removed element: ", e)
+#         print(stack)
+
+# n = int(input("limit of stack: "))
+# while True:
+#     print("Select the operation 1.push 2.pop 3.quit")
+#     choice = int(input())
+#     if choice==1:
+#         push()
+#     elif choice==2:
+#         pop_element()
+#     elif choice==3:
+#         break 
+#     else:
+#         print("Enter the correct operation!")
+
