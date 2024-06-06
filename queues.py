f98ee87fb444ab5c846c83c53e251dc738fa5747
Harry Monse <harry.monse1812@gmail.com>
2024-06-06 20:01:29 +0530

    implementing queue using list completed

--- queues.py
+++ queues.py
@@ -1,2 +1,68 @@
-# Queue
+# # Queue
+
+# queue = []                                                  # Implementing Queue using list
+# queue.append(10)
+# queue.append(20)
+# queue.append(30)
+# print(queue)
+
+# print(queue.pop(0))
+# print(queue.pop(0))
+# print(queue.pop(0))
+
+
+# queue = []                                                  # Implementing Queue using list                                                    
+# queue.insert(0, 10)
+# queue.insert(0, 20)
+# queue.insert(0, 30)
+# print(queue)
+
+# print(queue.pop())
+# print(queue.pop())
+# print(queue.pop())
+
+
+# queue = []                                                  # isEmpty
+# print(not queue)                                            
+
+
+# queue = []                                                  # Front and Rear element
+# queue.append(10)
+# queue.append(20)
+# queue.append(30)
+
+# print(queue[-1])
+# print(queue[0])
+
+
+
+# queue = []                                                  # Program
+# def enqueue() :
+#     element = input("Enter the element: ")
+#     queue.append(element)
+#     print(element, "is added to queue!")
+# def dequeue() :
+#     if not queue:
+#         print("queue is empty!")
+#     else:
+#         e = queue.pop(0)
+#         print("removed element: " , e)
+# def display() :
+#     print(queue)
+
+# while True:
+#     print("Select the operation 1.add 2.remove 3.show 4.3quit")
+#     choice = int(input())
+#     if choice==1:
+#         enqueue()
+#     elif choice==2:
+#         dequeue()
+#     elif choice==3:
+#         display()
+#     elif choice==4:
+#         break 
+#     else:
+#         print("Enter the correct operation!")
+
+
 
