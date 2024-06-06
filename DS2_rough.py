0187592e16fb6e1397c2cadd7cfd65d5384204ba
Harry Monse <harry.monse1812@gmail.com>
2024-06-07 00:17:34 +0530

    practicing sorting - selection sorting

--- DS2_rough.py
+++ DS2_rough.py
@@ -1,1 +1,14 @@
-#Stack and Queues
+# num = int(input("Enter a limit: "))                                             # Selection Sorting
+# list1 = [int(input("Enter number: ")) for x in range(num)]
+# print(list1)
+
+# for i in range(len(list1)-1):
+#     m_ind = i
+#     for j in range(i+1, len(list1)):
+#         if list1[j] < list1[m_ind]:
+#             m_ind = j
+#     if list1[i] != list1[m_ind]:
+#         list1[i] , list1[m_ind] = list1[m_ind], list1[i]
+    
+# print(list1)
+
