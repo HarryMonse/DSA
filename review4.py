c1418c2abde24a3264bebcbc62c60915944f52e7
Harry Monse <harry.monse1812@gmail.com>
2024-07-04 16:56:42 +0530

    dsa problems

--- review4.py
+++ review4.py
@@ -44,3 +44,21 @@
 # InsertionSort(my_list)
 # print(my_list)
 
+
+
+
+
+
+
+# def InsertionSort(my_list):                                                   # Practical Question 2
+#     for index in range(1, len(my_list)):
+#         current_element = my_list[index]
+#         pos = index
+#         while current_element < my_list[pos-1] and pos > 0:
+#             my_list[pos] = my_list[pos-1]
+#             pos = pos - 1
+#         my_list[pos] = current_element
+
+# my_list = [64, 34, 25, 12, 22, 11, 90]
+# InsertionSort(my_list)
+# print(my_list)
