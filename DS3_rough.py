8420e8b24204c2f1a0525035bde79b971b45e31c
Harry Monse <harry.monse1812@gmail.com>
2024-07-13 04:16:38 +0530

    Binary Search Tree

---
+++ DS3_rough.py
@@ -0,0 +1,11 @@
+class BST:                                                              # Binary Search Tree
+    def __init__(self,key) :
+        self.key = key
+        self.lchild = None
+        self.rchild = None
+
+root = BST(10)
+print(root.key)
+print(root.lchild)
+print(root.rchild)
+
