b28ad7517f1560c3edc7c5a9a115a2d50292b87e
Harry Monse <harry.monse1812@gmail.com>
2024-07-15 07:20:24 +0530

    Graph Practices

--- DS3_rough.py
+++ DS3_rough.py
@@ -10,15 +10,3 @@ print(root.lchild)
 print(root.rchild)
 
 
-class BST:                                                              # Binary Search Tree
-    def __init__(self,key) :
-        self.key = key
-        self.lchild = None
-        self.rchild = None
-
-root = BST(10)
-print(root.key)
-print(root.lchild)
-print(root.rchild)
-
-
